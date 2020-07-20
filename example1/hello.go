01f0cbdd3ec56df68852197d26a2914fd6ada294
Michael Mrowetz <michael.mrowetz@rangle.io>
2020-07-20 17:20:33 -0400

    fix example 1

--- example1/hello.go
+++ example1/hello.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Hello, Rangle")
-	wordPtr := flag.String("word", "some default value for word", "some description of word")
-	flag.Parse()
-	fmt.Println(*wordPtr)
 }
